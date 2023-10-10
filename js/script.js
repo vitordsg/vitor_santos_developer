2cc5c9392f07052d2e9d08a40910dd4784ebfeac
Vitor Santos <vitinho6.6@hotmail.com>
2023-10-09 23:15:45 -0300

    Inicio do portfolio feito

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 import mostrarMenuMobile from "./modules/mostrarMenuMobile.js";
 import removerMenuMobile from "./modules/removerMenuMobile.js";
+import sombraHeader from "./modules/sombraHeader.js";
 
+sombraHeader();
 mostrarMenuMobile();
 removerMenuMobile();
+
