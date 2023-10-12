2d2bbb2e79f9da8480e29245f7f4caa3e569e1cd
Vitor Santos <vitinho6.6@hotmail.com>
2023-10-12 01:55:09 -0300

    Seção de habilidades criada

--- js/modules/linkAtivoDeSecoesRolagem.js
+++ js/modules/linkAtivoDeSecoesRolagem.js
@@ -2,27 +2,22 @@ export default function linkAtivoDeSecoesRolagem() {
   const sections = document.querySelectorAll("section[id]");
 
   const scrollAtivo = () => {
-    const scrollDown = window.scrollY
-    
-    sections.forEach(current => {
-      const sectionHeight = current.offsetHeight
-      
-       const sectionTop = current.offsetTop - 58
-       
-       const sectionId = current.getAttribute('id')
-       
-       const sectionsClass = document.querySelector('.nav-menu a[href*=' + sectionId + ']')
-      console.log(sectionsClass)
+    const scrollDown = window.scrollY;
 
-        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
-          sectionsClass.classList.add('link-ativo')
-        }
+    sections.forEach((current) => {
+      const sectionHeight = current.offsetHeight;
+      const sectionTop = current.offsetTop - 258;
+      const sectionId = current.getAttribute("id");
+      const sectionsClass = document.querySelector(
+        ".nav-menu a[href*=" + sectionId + "]"
+      );
 
-        else {
-          sectionsClass.classList.remove('link-ativo')
-        }
-    })
-  }
-  window.addEventListener('scroll', scrollAtivo)
-  
+      if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+        sectionsClass.classList.add("link-ativo");
+      } else {
+        sectionsClass.classList.remove("link-ativo");
+      }
+    });
+  };
+  window.addEventListener("scroll", scrollAtivo);
 }
