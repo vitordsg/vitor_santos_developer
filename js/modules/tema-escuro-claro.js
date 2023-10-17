ca7685f193a938a3c401f402c5742d4836e4d368
Vitor dos Santos Gonçalves <vitinho6.6@hotmail.com>
2023-10-16 22:06:54 -0300

    Adicionado tema escuro e claro ao portfolio

--- js/modules/tema-escuro-claro.js
+++ js/modules/tema-escuro-claro.js
@@ -12,4 +12,20 @@ export default function temaEscuroClaro() {
   const obterIconeAtual = () => botaoTema.classList.contains(temaClaro) ? 'ri-moon-clear-line' : 'ri-sun-line'
 
   // Validamos se o usuário escolheu previamente um tema na página
+  if (temaSelecionado) {
+
+    // Se a validação for feita, perguntamos qual foi o problema para saber se ativamos ou desativamos o escuro
+    document.body.classList[temaSelecionado === 'escuro' ? 'add' : 'remove'](temaEscuro)
+    botaoTema.classList[iconeSelecionado === 'ri-moon-clear-line' ? 'add' : 'remove'](temaClaro)
+  }
+
+  botaoTema.addEventListener('click', () =>{
+    // Adiciona ou remove tema escuro / tema icone
+    document.body.classList.toggle(temaEscuro)
+    botaoTema.classList.toggle(temaClaro)
+
+    // Salvamos o tema e o ícone atual que o usuário escolheu
+    localStorage.setItem('tema-selecionado' , obterTemaAtual())
+    localStorage.setItem('icone-selecionado' , obterIconeAtual())
+  })
 }
