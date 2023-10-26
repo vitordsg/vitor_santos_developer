7163cf98f1f2437bf01b3546bbc4712f817271e2
Vitor dos Santos Gonçalves <vitinho6.6@hotmail.com>
2023-10-25 23:30:45 -0300

    Organizando os scripts

---
+++ js/modules/email.js
@@ -0,0 +1,37 @@
+export default function enviarEmail() {
+  const contatoForm = document.querySelector(".contato-form");
+  const contatoMensagem = document.querySelector(".contato-mensagem");
+
+  const enviarEmail = (e) => {
+    e.preventDefault();
+
+    // serviceID - templateID - #form - publickKey
+    emailjs
+      .sendForm(
+        "service_t4bamab",
+        "template_jug2h9v",
+        "#contato-form",
+        "8rhKa0NcK5Vyr-PDJ"
+      )
+      .then(
+        () => {
+          // Mostrar enviar mensagem
+          contatoMensagem.textContent = "Mensagem enviada com sucesso!✔️";
+
+          // Remover mensagem após 5 segundos
+          setTimeout(() => {
+            contatoMensagem.textContent = "";
+          }, 5000);
+
+          // Limpar as caixas de texto
+          contatoForm.reset();
+        },
+        () => {
+          // Mostrar Mensagem de Error!
+          contatoMensagem.textContent =
+            "Mensagem não enviada (ERRO de serviço ❌)";
+        }
+      );
+  };
+  contatoForm.addEventListener("submit", enviarEmail);
+}
--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ import sombraHeader from "./modules/sombraHeader.js";
 import botaoParaInicio from "./modules/botaoParaInicio.js";
 import linkAtivoDeSecoesRolagem from "./modules/linkAtivoDeSecoesRolagem.js";
 import temaEscuroClaro from "./modules/tema-escuro-claro.js";
+import enviarEmail from "./modules/email.js";
 
 linkAtivoDeSecoesRolagem();
 sombraHeader();
@@ -11,3 +12,18 @@ mostrarMenuMobile();
 removerMenuMobile();
 botaoParaInicio();
 temaEscuroClaro();
+enviarEmail();
+
+// Relevar Sections ao Scrollar com o mouse
+const sr = ScrollReveal( {
+  origin: 'top',
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
+  // reset: true // Animações repetem
+})
+
+sr.reveal(`.inicio-perfil`, {origin: 'right'})
+sr.reveal(`.inicio-nome, .inicio-info`, {origin: 'left'})
+
+// Relevar Sections ao Scrollar com o mouse
