f04eb641140f9ea14d9b780ac75e7276ee21bfbd
Vitor dos Santos Gonçalves <vitinho6.6@hotmail.com>
2024-06-20 20:32:59 -0300

    :lipstick: Pequenas alterações de animações e estilização do css

--- js/modules/removerMenuMobile.js
+++ js/modules/removerMenuMobile.js
@@ -4,6 +4,5 @@ export default function removerMenuMobile() {
     const navMenu = document.querySelector(".nav-menu");
     navMenu.classList.remove("mostrar-menu");
   };
-
   navLink.forEach((n) => n.addEventListener("click", linkAction));
 }
